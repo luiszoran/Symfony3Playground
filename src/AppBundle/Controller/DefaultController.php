77f2074035633307583ead8b9975016dd154ccc6
Luis Zoran Robaina Kotur <luiszoran@gmail.com>
2017-10-31 13:13:25 +0000

    Added controller, router through YAML and redirection

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,10 +13,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        //return $this->redirect($this->container->get("router")->getContext()->getBaseUrl().'/pruebas/es/Luis/11');
+        return $this->redirect($request->getBaseUrl().'/pruebas/es/Luis/11');
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        /*return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        ]);*/
     }
-    
+
 }
---
+++ src/AppBundle/Controller/PruebasController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PruebasController extends Controller
+{
+
+    public function indexAction(Request $request, $name, $page)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:pruebas:index.html.twig', [
+            'texto' => $name.' '.$page
+        ]);
+    }
+
+}
