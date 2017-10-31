40528304973890c5fa433ca8ab928a02c6c772ee
Luis Zoran Robaina Kotur <luiszoran@gmail.com>
2017-10-31 13:46:24 +0000

    Check POST parameters and added bundle

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -11,6 +11,10 @@ class PruebasController extends Controller
 
     public function indexAction(Request $request, $name, $page)
     {
+        var_dump($request->query->get("hola"));
+        var_dump($request->get("hola-post"));
+        die();
+
         // replace this example code with whatever you need
         return $this->render('AppBundle:pruebas:index.html.twig', [
             'texto' => $name.' '.$page
