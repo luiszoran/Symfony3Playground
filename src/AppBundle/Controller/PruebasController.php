903613284c4cf143eb6c0b0c032c448749fd2735
Luis Zoran Robaina Kotur <luiszoran@gmail.com>
2017-11-03 17:01:43 +0000

    Generate entities with doctrine and yaml, quick test of writing and reading from DB

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Curso;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,4 +27,36 @@ class PruebasController extends Controller
         ]);
     }
 
+    public function createAction(){
+        $curso = new Curso();
+        $curso->setTitulo("Curso de Symfony 3");
+        $curso->setDescripcion("Maravilloso curso de Symfony");
+        $curso->setPrecio(80);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($curso);
+        $flush = $em->flush();
+
+        if($flush != null){
+            echo "Error al crear el curso";
+        } else {
+            echo "Curso creado correctamente.";
+        }
+
+        die();
+    }
+
+    public function readAction(){
+        $em = $this->getDoctrine()->getManager();
+        $cursos_repo = $em->getRepository("AppBundle:Curso");
+        $cursos = $cursos_repo->findAll();
+
+        foreach($cursos as $curso){
+            echo $curso->getTitulo()."</br>";
+            echo $curso->getDescripcion()."</br>";
+            echo $curso->getPrecio()."</br></hr>";
+        }
+
+        die();
+    }
 }
