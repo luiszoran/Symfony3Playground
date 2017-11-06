101a89a67198c21fed2def62b9f761036fd28507
Luis Zoran Robaina Kotur <luiszoran@gmail.com>
2017-11-06 12:48:50 +0000

    Added form and validation

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -3,9 +3,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Curso;
+use AppBundle\Form\CursoType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class PruebasController extends Controller {
 
@@ -179,6 +181,46 @@ class PruebasController extends Controller {
             echo $curso->getTitulo() . "</br>";
             echo $curso->getDescripcion() . "</br>";
             echo $curso->getPrecio() . "</br></hr>";
+        }
+
+        die();
+    }
+
+    public function formAction(Request $request){
+        $curso = new Curso();
+        $form = $this->createForm(CursoType::class, $curso);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()){
+            $status = "Formulario válido";
+            $data = array(
+                "titulo" => $form->get("titulo")->getData(),
+                "descripcion" => $form->get("descripcion")->getData(),
+                "precio" => $form->get("precio")->getData()
+            );
+        }else{
+            $status = null;
+            $data = null;
+        }
+
+        return $this->render('AppBundle:pruebas:form.html.twig', [
+            'form' => $form->createView(),
+            'status' => $status,
+            'data' => $data
+        ]);
+    }
+
+    public function validarEmailAction($email){
+        $emailConstraint = new Assert\Email();
+        $emailConstraint->message = "Pase un correo correcto";
+
+        $error = $this->get("validator")->validate($email,$emailConstraint);
+
+        if (count($error) == 0){
+            echo "<h1>Correo válido";
+        } else{
+            echo $error[0]->getMessage();
         }
 
         die();
